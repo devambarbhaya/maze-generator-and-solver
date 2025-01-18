1eaaf605307884adb065a9a495bd014a821428a0
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-18 18:25:52 +0000

    add breaking the entrance and exit walls functionality

--- graphics.py
+++ graphics.py
@@ -4,7 +4,7 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("Maze Solver")
+        self.__root.title("Maze Generator and Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
