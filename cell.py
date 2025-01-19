c527273e5fea2ce0c254f786640836069d1a7d12
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-19 08:21:58 +0000

    Make the visuals prettier, change the colors, etc

--- cell.py
+++ cell.py
@@ -1,6 +1,5 @@
 from graphics import Line, Point
 
-
 class Cell:
     def __init__(self, win=None):
         self.has_left_wall = True
@@ -21,30 +20,37 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
+
+        wall_color = "#2f4f4f"  # DarkSlateGray
+        empty_color = "#f0f8ff"  # AliceBlue
+
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line)
+            self._win.draw_line(line, wall_color)
         else:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line, "white")
+            self._win.draw_line(line, empty_color)
+
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line)
+            self._win.draw_line(line, wall_color)
         else:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line, "white")
+            self._win.draw_line(line, empty_color)
+
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line)
+            self._win.draw_line(line, wall_color)
         else:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line, "white")
+            self._win.draw_line(line, empty_color)
+
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line)
+            self._win.draw_line(line, wall_color)
         else:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line, "white")
+            self._win.draw_line(line, empty_color)
 
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
@@ -55,9 +61,9 @@ class Cell:
         x_center2 = half_length2 + to_cell._x1
         y_center2 = half_length2 + to_cell._y1
 
-        fill_color = "red"
+        fill_color = "#ff6347"  # Tomato
         if undo:
-            fill_color = "gray"
+            fill_color = "#4682b4"  # SteelBlue
 
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
         self._win.draw_line(line, fill_color)
--- graphics.py
+++ graphics.py
@@ -6,7 +6,7 @@ class Window:
         self.__root = Tk()
         self.__root.title("Maze Generator and Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas = Canvas(self.__root, bg="#f0f8ff", height=height, width=width)  # AliceBlue background
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
 
@@ -18,14 +18,13 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
-        print("window closed...")
+        print("Window closed...")
 
     def close(self):
         self.__running = False
-        
+
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
-
 
 
 class Point:
@@ -35,9 +34,11 @@ class Point:
 
 
 class Line:
-    def __init__(self,p1,p2,):
+    def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=3
+        )
